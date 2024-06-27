d6fde3b07a0717bfe5211e1d274834c27dac675e
Ibnu Maksum <ibnumaksum@gmail.com>
2024-06-27 09:48:36 +0700

    First Upload

---
+++ WhatsappGateway.php
@@ -0,0 +1,318 @@
+<?php
+
+register_menu("Whatsapp Gateway", true, "whatsappGateway", 'AFTER_SETTINGS', 'glyphicon glyphicon-comment', '', '', ['Admin', 'SuperAdmin']);
+
+register_hook('send_whatsapp', 'whatsappGateway_hook_send_whatsapp');
+
+function whatsappGateway()
+{
+    global $ui, $config, $admin;
+    _admin();
+    $path = whatsappGateway_getPath();
+
+    if (empty($config['whatsapp_gateway_secret'])) {
+        r2(U . 'plugin/whatsappGateway_config', 'e', 'Please configure first');
+    }
+
+    $files = scandir($path);
+    $phones = [];
+    foreach ($files as $file) {
+        if (pathinfo($file, PATHINFO_EXTENSION) == 'nux') {
+            $phone = str_replace(".nux", "", $file);
+            $phones[] = $phone;
+        }
+    }
+
+    $ui->assign('phones', $phones);
+    $ui->assign('_title', 'Whatsap Gateway');
+    $ui->assign('_system_menu', 'plugin/whatsappGateway');
+    $admin = Admin::_info();
+    $ui->assign('_admin', $admin);
+    $ui->display('whatsappGateway.tpl');
+}
+
+function whatsappGateway_config()
+{
+    global $ui;
+    _admin();
+
+    if (!empty(_post('whatsapp_gateway_url')) || !empty(_post('whatsapp_gateway_secret'))) {
+        $d = ORM::for_table('tbl_appconfig')->where('setting', 'whatsapp_gateway_url')->find_one();
+        if ($d) {
+            $d->value = _post('whatsapp_gateway_url');
+            $d->save();
+        } else {
+            $d = ORM::for_table('tbl_appconfig')->create();
+            $d->setting = 'whatsapp_gateway_url';
+            $d->value = _post('whatsapp_gateway_url');
+            $d->save();
+        }
+        $d = ORM::for_table('tbl_appconfig')->where('setting', 'whatsapp_gateway_secret')->find_one();
+        if ($d) {
+            $d->value = _post('whatsapp_gateway_secret');
+            $d->save();
+        } else {
+            $d = ORM::for_table('tbl_appconfig')->create();
+            $d->setting = 'whatsapp_gateway_secret';
+            $d->value = _post('whatsapp_gateway_secret');
+            $d->save();
+        }
+        r2(U . 'plugin/whatsappGateway_config', 's', 'Configuration saved');
+    }
+    $ui->assign('_title', 'Whatsap Gateway Configuration');
+    $ui->assign('_system_menu', 'plugin/whatsappGateway');
+    $admin = Admin::_info();
+    $ui->assign('_admin', $admin);
+    $ui->assign('menu', 'config');
+    $ui->display('whatsappGateway.tpl');
+}
+
+
+function whatsappGateway_login()
+{
+    global $ui;
+    _admin();
+
+    $phone = alphanumeric(_get('p'));
+    $path = whatsappGateway_getPath();
+    if (empty($phone)) {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Not Found');
+    }
+    if (!file_exists("$path$phone.nux")) {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Not Found.');
+    }
+    $json = json_decode(file_get_contents("$path$phone.nux"), true);
+    if (!isset($json['jwt'])) {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Not Connected');
+    }
+    if ($json['jwt'] == '') {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Not Connected.');
+    }
+    if (strlen($json['jwt']) > 4 && substr($json['jwt'], 0, 5) == 'Error') {
+        // repeat request
+        $json['jwt'] = whatsappGateway_getJwtApi($phone);
+        if (strlen($json['jwt']) > 4 && substr($json['jwt'], 0, 5) == 'Error') {
+            r2(U . 'plugin/whatsappGateway', 'e', $json['jwt']);
+        } else {
+            file_put_contents("$path$phone.nux", json_encode($json));
+        }
+    }
+
+    $result = whatsappGateway_loginApi($json['jwt']);
+    if (strlen($result)) {
+        if (substr($result, 0, 1) == '{') {
+            $json = json_decode($result, true);
+            if (!empty($json['data']['paircode'])) {
+                $message = $json['message'] . '<br><br>';
+                $message .= '<h1>' . $json['data']['paircode'] . '</h1><br>';
+                $message .= 'Timeout in ' . $json['data']['timeout'] . ' Second(s)<br>';
+            } else {
+                $message = $json['message'];
+            }
+        } else {
+            $message = $result;
+        }
+    } else {
+        $message = $result;
+    }
+    if (trim($message) == 'WhatsApp Client is Reconnected') {
+        $message = '<span class="label label-success">Logged in</span>';
+    }
+    $ui->assign('message', $message);
+    $admin = Admin::_info();
+    $ui->assign('_admin', $admin);
+    $ui->assign('menu', 'login');
+    $ui->display('whatsappGateway.tpl');
+}
+
+function whatsappGateway_addPhone()
+{
+    _admin();
+    $path = whatsappGateway_getPath();
+    $phone = alphanumeric(_post("phonenumber"));
+    if (empty($phone)) {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Phone not found');
+    }
+    if (file_exists("$path$phone.nux")) {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Phone already exists');
+    }
+    $json['jwt'] = whatsappGateway_getJwtApi($phone);
+    file_put_contents("$path$phone.nux", json_encode($json));
+    if (file_exists("$path$phone.nux")) {
+        r2(U . 'plugin/whatsappGateway', 's', 'Phone Added');
+    } else {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Phone Failed to add');
+    }
+}
+
+function whatsappGateway_send()
+{
+    global $config;
+    $to = alphanumeric(_req('to'));
+    $msg = _req('msg');
+    $secret = _req('secret');
+    if ($secret != md5($config['whatsapp_gateway_secret'])) {
+        showResult(false, 'Invalid secret');
+    }
+    $result  = whatsappGateway_hook_send_whatsapp([
+        'phone' => $to,
+        'txt' => $msg
+    ]);
+    $json = json_decode($result, true);
+    if ($json) {
+        showResult(true, '', $json);
+    } else {
+        showResult(false, '', $result);
+    }
+}
+
+function whatsappGateway_hook_send_whatsapp($data = [])
+{
+    global $config;
+    list($phone, $txt) = $data;
+    if (!empty($config['whatsapp_gateway_url'])) {
+        $path = whatsappGateway_getPath();
+        $files = scandir($path);
+        $was = [];
+        foreach ($files as $file) {
+            if (pathinfo($file, PATHINFO_EXTENSION) == 'nux') {
+                $was[] = $file;
+            }
+        }
+        $wa = $was[rand(0, count($was) - 1)];
+        $json = json_decode(file_get_contents("$path$wa"), true);
+        $url = $config['whatsapp_gateway_url'];
+        return Http::postData(
+            $url . '/send/text',
+            array('msisdn' => $phone, 'message' => $txt),
+            [
+                'Content-Type: application/x-www-form-urlencoded',
+                "Authorization: Bearer $json[jwt]"
+            ]
+        );
+    }
+}
+
+function whatsappGateway_delPhone()
+{
+    _admin();
+    $path = whatsappGateway_getPath();
+    $phone = alphanumeric(_get('p'));
+    if (empty($phone)) {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Phone not found');
+    }
+    if (!file_exists("$path$phone.nux")) {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Phone not exists');
+    }
+    if (unlink("$path$phone.nux")) {
+        r2(U . 'plugin/whatsappGateway', 's', 'Phone Deleted');
+    } else {
+        r2(U . 'plugin/whatsappGateway', 'e', 'Phone Failed to Delete');
+    }
+}
+
+function whatsappGateway_status()
+{
+    $phone = alphanumeric(_get('p'));
+    $path = whatsappGateway_getPath();
+    if (empty($phone)) {
+        die('<span class="label label-danger">Not Found</span>');
+    }
+    if (!file_exists("$path$phone.nux")) {
+        die('<span class="label label-danger">Not Found.</span>');
+    }
+
+    $json = json_decode(file_get_contents("$path$phone.nux"), true);
+    if (!isset($json['jwt'])) {
+        die('<span class="label label-danger">Not Connected</span>');
+    }
+    if ($json['jwt'] == '') {
+        die('<span class="label label-danger">Not Connected.</span>');
+    }
+    if (strlen($json['jwt']) > 4 && substr($json['jwt'], 0, 5) == 'Error') {
+        // repeat request
+        $json['jwt'] = whatsappGateway_getJwtApi($phone);
+        if (strlen($json['jwt']) > 4 && substr($json['jwt'], 0, 5) == 'Error') {
+            die('<span class="label label-danger">' . $json['jwt'] . '</span>');
+        } else {
+            file_put_contents("$path$phone.nux", json_encode($json));
+        }
+    }
+
+    $result = whatsappGateway_loginApi($json['jwt']);
+    if (strlen($result)) {
+        if (substr($result, 0, 1) == '{') {
+            $json = json_decode($result, true);
+            if (!empty($json['data']['paircode'])) {
+                $message = $json['message'] . '<br><br>';
+                $message .= '<h1>' . $json['data']['paircode'] . '</h1><br>';
+                $message .= 'Timeout in ' . $json['data']['timeout'] . ' Second(s)<br>';
+            } else {
+                $message = $json['message'];
+            }
+        } else {
+            $message = $result;
+        }
+    } else {
+        $message = $result;
+    }
+    if (trim($message) == 'WhatsApp Client is Reconnected') {
+        die('<span class="label label-success">Logged in</span>');
+    } else {
+        die('<span class="label label-danger">Not Logged in</span>');
+    }
+    die();
+}
+
+function whatsappGateway_getPath()
+{
+    global $UPLOAD_PATH;
+    $path = $UPLOAD_PATH . DIRECTORY_SEPARATOR . "whatsapp" . DIRECTORY_SEPARATOR;
+    if (!file_exists($path)) {
+        mkdir($path);
+    }
+    return $path;
+}
+
+
+function whatsappGateway_getJwtApi($phone)
+{
+    global $config;
+    $url = $config['whatsapp_gateway_url'] . '/auth';
+    $result = Http::getData(
+        $url,
+        [
+            "Authorization: Basic " . base64_encode($phone . ":" . $config['whatsapp_gateway_secret'])
+        ]
+    );
+    $json = json_decode($result, true);
+    if ($json['status'] == 200) {
+        return $json['data']['token'];
+    } else {
+        if (isset($json['message'])) {
+            return "Error: " . $json['message'];
+        } else {
+            return "Error: " . $result;
+        }
+    }
+}
+
+
+function whatsappGateway_loginApi($jwt_whatsapp)
+{
+    global $config;
+    if (isset($_GET['pair'])) {
+        $url = $config['whatsapp_gateway_url'] . '/login/pair';
+    } else {
+        $url = $config['whatsapp_gateway_url'] . '/login';
+    }
+    $result = Http::postData(
+        $url,
+        [],
+        [
+            'Content-Type: application/x-www-form-urlencoded',
+            "Authorization: Bearer $jwt_whatsapp"
+        ]
+    );
+    return $result;
+}
---
+++ ui/whatsappGateway.tpl
@@ -0,0 +1,163 @@
+{include file="sections/header.tpl"}
+
+{if $menu == 'login'}
+
+    <form class="form" method="post" role="form">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">
+                            Login Whatsapp
+                        </h3>
+                    </div>
+                    <div class="box-body with-border text-center">
+                        {$message}
+                    </div>
+                    <div class="box-footer">
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <a class="btn btn-default btn-sm btn-block" href="{$_url}plugin/whatsappGateway">back</a>
+                            </div>
+                            <div class="col-xs-8">
+                                <button class="btn btn-primary btn-sm btn-block" type="submit">Check Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+{elseif $menu == 'config'}
+
+    <form class="form" method="post" role="form" action="{$_url}plugin/whatsappGateway_config">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">
+                            Configuration
+                        </h3>
+                    </div>
+                    <div class="box-body with-border">
+                        <div class="form-group">
+                            <label>Server URL</label>
+                            <input type="text" class="form-control" name="whatsapp_gateway_url"
+                                value="{$_c['whatsapp_gateway_url']}" required placeholder="http://localhost:3000">
+                            <a href="https://github.com/dimaskiddo/go-whatsapp-multidevice-rest" class="pull-right"
+                                target="_blank">Go WhatsApp Multi-Device</a>
+                        </div>
+                        <div class="form-group">
+                            <label>Auth Basic Password</label>
+                            <input type="text" class="form-control" name="whatsapp_gateway_secret" required
+                                placeholder="AUTH_BASIC_PASSWORD" value="{$_c['whatsapp_gateway_secret']}">
+                            <span class="text-muted pull-right">AUTH_BASIC_PASSWORD From .env</span>
+                        </div>
+                    </div>
+                    <div class="box-footer">
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <a class="btn btn-default btn-sm btn-block" href="{$_url}plugin/whatsappGateway">back</a>
+                            </div>
+                            <div class="col-xs-8">
+                                <button class="btn btn-primary btn-sm btn-block" type="submit">Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+{else}
+    <div class="row">
+        <div class="col-md-4">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">
+                        <i class="glyphicon glyphicon-plus"></i>
+                        Add Phone
+                    </h3>
+                    <div class="box-tools pull-right">
+                        <a href="{$_url}plugin/whatsappGateway_config" class="btn btn-box-tool" data-toggle="tooltip"
+                            data-placement="top" title="Configuration"><i class="glyphicon glyphicon-cog"></i></a>
+                    </div>
+                </div>
+                <div class="box-body with-border">
+                    <form class="form-horizontal" method="post" role="form" action="{$_url}plugin/whatsappGateway_addPhone">
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <label>{Lang::T('Phone Number')}</label>
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="basic-addon1"><i
+                                            class="glyphicon glyphicon-phone"></i></span>
+                                    <input type="text" class="form-control" name="phonenumber" required
+                                        placeholder="{$_c['country_code_phone']} {Lang::T('Phone Number')}">
+                                </div>
+                                <span class="pull-right">Use Country Code as whatsapp need it</span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <button class="btn btn-success btn-block btn-sm" type="submit">Add</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Registered Phone</h3>
+                    <div class="box-tools pull-right">
+                        <a href="{$_url}plugin/whatsappGateway_config" class="btn btn-box-tool" data-toggle="tooltip"
+                            data-placement="top" title="Configuration"><i class="glyphicon glyphicon-cog"></i></a>
+                    </div>
+                </div>
+                <table class="table table-condensed table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Phone Number</th>
+                            <th>Status</th>
+                            <th colspan="2">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {foreach $phones as $phone}
+                            <tr>
+                                <td>{$phone}</td>
+                                <td api-get-text='{$_url}plugin/whatsappGateway_status&p={$phone}'><span
+                                        class="label label-default">&nbsp;</span></td>
+                                <td>
+                                    <a href="{$_url}plugin/whatsappGateway_login&p={$phone}"
+                                        class="btn btn-xs btn-primary">QRCode</a>
+                                    <a href="{$_url}plugin/whatsappGateway_login&p={$phone}&pair"
+                                        class="btn btn-xs btn-primary">Paircode</a>
+                                </td>
+                                <td>
+                                    <a href="{$_url}plugin/whatsappGateway_delPhone&p={$phone}" class="btn btn-xs btn-danger"
+                                        onclick="return confirm('Remove {$phone}?')">Remove</a>
+                                </td>
+                            </tr>
+                        {/foreach}
+                    </tbody>
+                </table>
+            </div>
+            <div class="bs-callout bs-callout-warning well">
+                <h4>API To send directly</h4>
+                <input type="text" class="form-control" readonly onclick="this.select();"
+                    value="{$_url}plugin/whatsappGateway_send&to=[number]&msg=[text]&secret={md5($_c['whatsapp_gateway_secret'])}">
+            </div>
+        </div>
+    </div>
+{/if}
+
+<div class="bs-callout bs-callout-warning well">
+    <h4>Sending WhatsApp</h4>
+    <p>If you put multiple number, it will send random to any existed phone number. even if it not logged in to
+        WhatsApp.</p>
+    <p><b>Empty Whatsapp Server URL in PHPNuxBill configuration</b>, this plugin will overide sending WhatsApp.</p>
+    <p>This plugin only support <a href="https://github.com/dimaskiddo/go-whatsapp-multidevice-rest" target="_blank">Go
+            WhatsApp Multi-Device</a>
+</div>
+{include file="sections/footer.tpl"}
